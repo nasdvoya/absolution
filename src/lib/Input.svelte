981fbe640193a3ffcd2c37869348f7e4f57de1ac
alexandr <nasdvoya@proton.me>
2025-06-25 00:58:17 +0100

    Input setup

---
+++ src/lib/Input.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let {
+		placeholder = '',
+		required = false,
+		pattern = '^\\d{9}$',
+		value = $bindable() as string
+	} = $props();
+
+	let isValid = $state(true);
+
+	// Debug tools
+	$inspect(isValid).with(console.trace);
+	$inspect(value).with(console.trace);
+
+	// Validation
+	function validate() {
+		if (required && value.trim() === '') {
+			isValid = false;
+		} else if (pattern) {
+			const regex = new RegExp(pattern);
+			isValid = regex.test(value);
+		} else {
+			isValid = true;
+		}
+	}
+</script>
+
+<div>
+	<input class="input focus:outline-none" {placeholder} {required} onblur={validate} oninput={validate} bind:value />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+    import Input from '$lib/Input.svelte';
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+	<div class="grid grid-cols-2 gap-x-8 gap-y-4 text-left">
+		<div>
+			<label class="label">Email</label>
+			<input type="email" class="input" placeholder="Email" />
+
+			<label class="label">Password</label>
+			<input type="password" class="input" placeholder="Password" />
+
+			<button class="btn btn-neutral mt-4">Login</button>
+		</div>
+
+		<div>
+			<label class="label">Email</label>
+			<input type="email" class="input" placeholder="Email" />
+
+			<label class="label">Password</label>
+			<input type="password" class="input" placeholder="Password" />
+
+			<button class="btn btn-neutral mt-4">Login</button>
+            <Input />
+		</div>
+	</div>
+</fieldset>
